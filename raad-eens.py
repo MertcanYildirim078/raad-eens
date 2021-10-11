e699493c06b86ac4528a2af5ec04b279fd15604e
MertcanYildirim078 <99068314@mydavinci.nl>
2021-10-11 10:17:49 +0200

    'raadeenszonderrondes

--- raad-eens.py
+++ raad-eens.py
@@ -7,27 +7,36 @@ ready = input('Ben je klaar om te spelen? j/n ')
 if ready == 'n':
     print('Bye!')
 else:
+    print('Ronde 1')
     geraden = False
     print(teradengetal)
-    while geraden == False:
+    while geraden == False and tries <10:
+        tries = gok == input
         gok = int(input('Gok een getal '))
         if gok > teradengetal:
             print('Het getal is kleiner')
-        elif gok < teradengetal:
+        elif gok < teradengetal: 
             print('Het getal is groter')
 
-        if abs(gok - teradengetal) <50:
-            print('Je bent warm!')
         if abs(gok - teradengetal) <20:
             print('Je bent heel warm!')
-
+        elif abs(gok - teradengetal) <50:
+            print('Je bent warm!')
+        if geraden == True or tries >10:
+            ronde += 1
+            stoppen = input('Wil je doorgaan? n/j ')
+            if stoppen == 'n':
+                print('Bye!')
+            else:
+                pass
         if ronde == 2:
-            break
+            break 
         if gok == teradengetal:
             geraden = True  
         if geraden == True:
             print('Gefeliciteerd u heeft het geraden!')
             score += 1
+        print(score)
 
 
     
