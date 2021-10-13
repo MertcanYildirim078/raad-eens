b33eb9e43b5b1a4d01c779dda34de8c23a16abeb
MertcanYildirim078 <99068314@mydavinci.nl>
2021-10-13 10:07:55 +0200

    'raadeensaf'

--- raad-eens.py
+++ raad-eens.py
@@ -7,12 +7,23 @@ ready = input('Ben je klaar om te spelen? j/n ')
 if ready == 'n':
     print('Bye!')
 else:
-    print('Ronde 1')
+    print('ronde:', ronde)
     geraden = False
-    print(teradengetal)
-    while geraden == False and tries <10:
-        tries = gok == input
+    tries = 10
+    while geraden == False or tries >0 or Doorgaan1 == 'j' or Doorgaan2 == 'j':
         gok = int(input('Gok een getal '))
+        if gok != teradengetal:
+            tries -= 1
+        if tries == 0:
+            Doorgaan1 = input('Helaas ben je gefaalt, doorgaan? n/j ')
+            if Doorgaan1 == 'n':
+                print('Bye!')
+                break
+            else:
+                teradengetal = random.randint(1,1000)
+                tries = 10
+                ronde += 1
+                print ('ronde:', ronde)
         if gok > teradengetal:
             print('Het getal is kleiner')
         elif gok < teradengetal: 
@@ -22,21 +33,23 @@ else:
             print('Je bent heel warm!')
         elif abs(gok - teradengetal) <50:
             print('Je bent warm!')
-        if geraden == True or tries >10:
-            ronde += 1
-            stoppen = input('Wil je doorgaan? n/j ')
-            if stoppen == 'n':
-                print('Bye!')
-            else:
-                pass
-        if ronde == 2:
-            break 
+        
         if gok == teradengetal:
             geraden = True  
-        if geraden == True:
+        if geraden == True and tries >0:
             print('Gefeliciteerd u heeft het geraden!')
             score += 1
-        print(score)
-
-
-    
+        print('Huidige score: ', score)
+        if geraden == True:
+            Doorgaan2 = input('Wil je nog doorgaan? n/j ')
+            if Doorgaan2 == 'n':
+                print('Bye!')
+                break
+            else:
+                teradengetal = random.randint(1, 1000)
+                tries = 10
+                ronde += 1
+                print ('ronde:', ronde)
+            if ronde >20:
+                print('Game Over! Huidige score:', score)
+                break
